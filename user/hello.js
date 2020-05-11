fb2c2ff67295c9e8579088f319c0ef1d039b0043
Glitch (ecs162-login-demo) <none>
2020-05-11 04:08:20 +0000

    🙆🏁 Checkpoint ./user/hello.js:13448247/44 ./user/hello.html:13448247/151

--- user/hello.js
+++ user/hello.js
@@ -1,3 +1,2 @@
 let msg = document.getElementById("cookieMessage");
-msg.textContent = msg.textContent+document.cookie;
-console.log(document.cookie);
+msg.textContent = msg.textContent+decodeURI(document.cookie);
