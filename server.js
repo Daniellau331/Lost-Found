f798218ad2426a4d576e7b5719056fbba62cfc54
Glitch (ylx-laf-project) <none>
2020-06-04 03:26:25 +0000

    🏔🐑 Checkpoint ./server.js:14032382/2

--- server.js
+++ server.js
@@ -160,8 +160,6 @@ app.get('/user/logoff',
 );
 
 
-
-
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
