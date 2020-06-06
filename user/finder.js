0cae9081e502b1c8569cd892c78d065106483ae8
Glitch (ylx-laf-project) <none>
2020-06-06 17:27:35 +0000

    📅🎬 Checkpoint ./user/finder.js:14009739/2391

--- user/finder.js
+++ user/finder.js
@@ -3,29 +3,33 @@ var coords, map, marker, infowindow;
 
 // DYNAMIC LOAD MAPS API
 // https://developers.google.com/maps/documentation/javascript/tutorial#Loading_the_Maps_API
-var script = document.createElement('script');
+var script = document.createElement("script");
 
 // UNCOMMENT THIS
-script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCDVK45xZOTksaRZ3eC4lyQjOy5xGsHOzs&callback=initMap';
+script.src =
+  "https://maps.googleapis.com/maps/api/js?key=AIzaSyCDVK45xZOTksaRZ3eC4lyQjOy5xGsHOzs&callback=initMap";
 script.defer = true;
 script.async = true;
 
-
-
-function initMap()
-{
+function initMap() {
   // SET MAP PROPERTIES
   var mapProp = {
     center: new google.maps.LatLng(38.5367859, -121.7553711), // USE USER GPS LOCATION
-    zoom: 15, // LARGER IS ZOOM IN
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    zoom: 15 // LARGER IS ZOOM IN
   };
 
   // INIT MAP
   map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-  
+
   marker = new google.maps.Marker();
-  marker.setMap(null);
+  marker.setDraggable(true);
+  marker.setPosition(map.center);
+  marker.setMap(map);
+
+  
+  marker.addListener("dragend", function() {
+    map.setCenter(marker.getPosition());
+  });
 }
 
 // Append the 'script' element to 'head'
