cfbfedc738e9287f158af3d6ec9c5b852c83c885
Glitch (ylx-laf-project) <none>
2020-06-07 12:50:42 +0000

    👰🎣 Checkpoint ./user/script.js:14032382/1604

--- user/script.js
+++ user/script.js
@@ -21,18 +21,42 @@ document.getElementById("attachment").addEventListener("change", function(){
   formData.append('newImage',selectedFile, selectedFile.name);
   
   // build an HTTP request data structure
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", "/upload", true);
-    xhr.onloadend = function(e) {
-      
-    // let newImage = document.querySelector("#cardImg");
-      // newImage.src = "https://quickest-like-attraction.glitch.me/images/"+selectedFile.name;
-      // newImage.style.display = 'block';
-      // document.querySelector('.image').classList.remove('upload');
-      // document.querySelector('.btn').textContent = 'Replace Image';
-      // sendGetRequest(selectedFile);
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "/upload", true);
+  xhr.onloadend = function(e) {
+    console.log("POST: upload");
+    console.log(xhr.responseText);
+    sendGetRequest(selectedFile);
   }
   
   // actually send the request
   xhr.send(formData);
 });
+
+
+// sends an AJAX request asking the server 
+function sendGetRequest(selectedFile) {
+  let xhr = new XMLHttpRequest;
+  // it's a GET request, it goes to URL /seneUploadToAPI
+  xhr.open("GET","sendUploadToAPI");
+  
+  // Add an event listener for when the HTTP response is loaded
+  // xhr.addEventListener("load", function() {
+  //     if (xhr.status == 200) {  // success
+  //       console.log("MESSSSSSAGE"+xhr.responseText);
+  //       showMsg("goodMessage",xhr.responseText);
+  //     } else { // failure
+  //       showMsg("badMessage",xhr.responseText);
+  //     }
+  // });
+  xhr.onloadend = function(e){
+    // let newImage = document.querySelector("#cardImg");
+    // newImage.src = "http://ecs162.org:3000/images/zroyu/"+selectedFile.name;
+    // newImage.style.display = 'block';
+    // document.querySelector('.image').classList.remove('upload');
+    // document.querySelector('.btn').textContent = 'Replace Image';
+  }
+  
+  // Actually send request to server
+  xhr.send();
+}
