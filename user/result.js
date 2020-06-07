a1e92cde678c2e4dcc7268d44295a41f8482e5c4
Glitch (ylx-laf-project) <none>
2020-06-07 16:48:33 +0000

    🍠📚 Checkpoint ./user/result.js:14032382/763 ./user/script.js:14009739/4766

--- user/result.js
+++ user/result.js
@@ -1,11 +1,16 @@
 window.onload = function () {
-  var url = document.location.href,
+  var url = decodeURI(document.location.href),
         params = url.split('?')[1].split('&'),
         data = {}, tmp;
-    for (var i = 0, l = params.length; i < l; i++) {
-         tmp = params[i].split('=');
-         data[tmp[0]] = tmp[1];
-    }
-    console.log(data);
-    document.getElementById('searchResult').innerHTML = data.location1;
+  for (var i = 0, l = params.length; i < l; i++) {
+       tmp = params[i].split('=');
+       data[tmp[0]] = tmp[1];
+  }
+  console.log(data);
+  const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let month1 = data.date1.split('-')[1];
+  let month2 = data.date2.split('-')[1];
+  let day1 = data.date1.split('-')[1];
+  let day2 = data.date2.split('-')[1];
+  document.getElementById('searchResult').innerHTML = data.date1 + "-" + data.date2 + "," + data.category1 + "," + data.location1;
 }
