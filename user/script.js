cee1982ab062781b23272b70f5cc1d5bf1a8a06e
Glitch (ylx-laf-project) <none>
2020-06-07 03:32:40 +0000

    🚵🐟 Checkpoint ./user/script.js:14032382/1096

--- user/script.js
+++ user/script.js
@@ -13,3 +13,27 @@ document.getElementById("searchBar").addEventListener("click", function(){
   search.className = "show";
 });
 
+document.getElementById("attachment").addEventListener("change", function(){
+  // get the file with the file dialog box
+  const selectedFile = document.querySelector('#imgUpload').files[0];
+  // store it in a FormData object
+  const formData = new FormData();
+  formData.append('newImage',selectedFile, selectedFile.name);
+  
+  // build an HTTP request data structure
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "/upload", true);
+    xhr.onloadend = function(e) {
+      
+    // let newImage = document.querySelector("#cardImg");
+      // newImage.src = "https://quickest-like-attraction.glitch.me/images/"+selectedFile.name;
+      // newImage.style.display = 'block';
+      // document.querySelector('.image').classList.remove('upload');
+      // document.querySelector('.btn').textContent = 'Replace Image';
+      // sendGetRequest(selectedFile);
+  }
+  
+  // actually send the request
+  xhr.send(formData);
+  
+});
